272b7bf79dbcbc3914a8abc9cea5ed7fa0a6b420
Sebastian Skaba <162018809+SecDev7@users.noreply.github.com>
2024-05-04 12:01:22 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,36 @@
+import company.beans.Person;
+import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+import company.interfaces.Messenger;
+import company.utils.MathUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Person[] people = new Person[5];
+        
+      
+        final int b = 10;
+
+        try {
+          
+            people[0] = new Person("Jan Kowalski", 30);
+            people[1] = new Person("Anna Nowak", 25);
+            people[2] = new Person("Piotr Wiśniewski", 40);
+            people[3] = new Person("Katarzyna Mazur", 22);
+            people[4] = new Person("Michał Wojcik", 35);
+
+            
+            Messenger messenger = new EmailMessenger();
+
+        
+            for (Person person : people) {
+                int agePlusB = MathUtils.add(person.getAge(), b);
+                messenger.sendMessage(person.getName() + " wiek + 10: " + agePlusB);
+            }
+
+        } catch (InvalidAgeException e) {
+            System.err.println("Podano nieprawidłowy wiek: " + e.getMessage());
+        }
+    }
+}
